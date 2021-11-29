8bf19c9ab1f17ce4e567e6706a02afb935a45297
Raymond <raymond@philippinedev.com>
2021-11-29 23:11:45 +0800

    enabled the human part in the game

---
+++ assets/javascript/main.js
@@ -0,0 +1,93 @@
+let blackJackGame = {
+  you: { scoreSpan: "#your-score", div: "#your-box", score: 0 },
+  dealer: { scoreSpan: "#dealer-score", div: "#dealer-box", score: 0 },
+  cards: ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"],
+  cardsValue: {
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+    10: 10,
+    J: 10,
+    Q: 10,
+    K: 10,
+    A: [1, 11],
+  },
+};
+
+const YOU = blackJackGame["you"];
+const DEALER = blackJackGame["dealer"];
+const hitSound = new Audio("assets/sounds/swish.m4a");
+
+document.querySelector("#hit-button").addEventListener("click", blackJackHit);
+document.querySelector("#deal-button").addEventListener("click", blackJackDeal);
+
+function blackJackHit() {
+  let card = randomCard();
+  showCard(card, YOU);
+  updateScore(card, YOU);
+  showScore(YOU);
+  console.log(YOU["score"]);
+}
+
+function randomCard() {
+  let cardPicker = Math.floor(Math.random() * 13);
+  return blackJackGame["cards"][cardPicker];
+}
+
+function showCard(card, player) {
+  if (player["score"] <= 21) {
+    let cardImage = document.createElement("img");
+    cardImage.src = `assets/images/${card}.png`;
+    document.querySelector(player["div"]).appendChild(cardImage);
+    hitSound.play();
+  }
+}
+
+function blackJackDeal() {
+  let yourImages = document.querySelector("#your-box").querySelectorAll("img");
+  let dealerImages = document
+    .querySelector("#dealer-box")
+    .querySelectorAll("img");
+
+  for (i = 0; i < yourImages.length; i++) {
+    yourImages[i].remove();
+  }
+  for (i = 0; i < dealerImages.length; i++) {
+    dealerImages[i].remove();
+  }
+
+  YOU["score"] = 0;
+  DEALER["score"] = 0;
+
+  document.querySelector("#your-score").textContent = 0;
+  document.querySelector("#dealer-score").textContent = 0;
+  document.querySelector("#your-score").style.color = "white";
+  document.querySelector("#dealer-score").style.color = "white";
+}
+
+function updateScore(card, player) {
+  if (card === "A") {
+    if (player["score"] + blackJackGame["cardsValue"][card][1] <= 21) {
+      player["score"] += blackJackGame["cardsValue"][card][1];
+    } else {
+      player["score"] += blackJackGame["cardsValue"][card][0];
+    }
+  } else {
+    player["score"] += blackJackGame["cardsValue"][card];
+  }
+}
+
+function showScore(player) {
+  if (player["score"] > 21) {
+    document.querySelector(player["scoreSpan"]).textContent = "BUST!";
+    document.querySelector(player["scoreSpan"]).style.color = "red";
+  } else {
+    document.querySelector(player["scoreSpan"]).textContent = player["score"];
+    document.querySelector(player["scoreSpan"]).style.color = "yellow";
+  }
+}
